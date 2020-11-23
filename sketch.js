6aa967ce3df2384a739bf18999d462f2992fae1d
Anmol785 <70816117+Anmol785@users.noreply.github.com>
2020-11-23 19:07:33 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
-var car , wall;
+var bullet , wall;
 var speed , weight;
+var thickness;
 
 function setup() {
   createCanvas(1600,400);
@@ -7,13 +8,14 @@ function setup() {
 
 speed = random(55,90);
 weight = random(400,1500);
+thickness=random(22,83);
 
-car=createSprite(50,200,50,50);
-car.shapeColor = color(255);
-wall=createSprite(1500,200,60,height/2);
+bullet=createSprite(50,200,10,50);
+bullet.shapeColor = color(255);
+wall=createSprite(1200,200,thickness,height/2);
 wall.shapeColor=color(80,80,80);
 
-car.velocityX=speed;
+bullet.velocityX=speed;
 
 
 }
@@ -21,22 +23,18 @@ car.velocityX=speed;
 function draw() {
   background(0);  
 
-  if (wall.x-car.x<(car.width+wall.width)/2)
+  if (wall.x-bullet.x<(bullet.width+wall.width)/2)
 {
-  car.velocityX=0;
+  bullet.velocityX=0;
 
-  var deformation=0.5 * weight * speed * speed/22500;
-  if(deformation>180)
+  var damage=0.5 * bulletweight * bulletspeed * bulletspeed/thickness*thickness*thickness;
+  if(damage>10)
   {
-    car.shapeColor=color(255,0,0);
+    bullet.shapeColor=color(255,0,0);
   }
-  if(deformation<180 && deformation>100)
+  if(deformation<10)
   {
-    car.shapeColor=color(230,230,0);
-  }
-  if(deformation<100)
-  {
-    car.shapeColor=color(0,255,0);
+    bullet.shapeColor=color(0,255,0);
   }
 }
 
